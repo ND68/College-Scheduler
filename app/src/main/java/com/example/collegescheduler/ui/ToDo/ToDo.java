7ec22070322bd8aeffcaebefed9397f8c62e506b
gkhandelwal7 <govindnarain.sk@gmail.com>
2024-01-26 11:03:33 -0500

    Added ToDo
    Changed strings.xml and navigation.

---
+++ app/src/main/java/com/example/collegescheduler/ui/ToDo/ToDo.java
@@ -0,0 +1,38 @@
+package com.example.collegescheduler.ui.ToDo;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.collegescheduler.R;
+
+public class ToDo extends Fragment {
+
+    private ToDoViewModel mViewModel;
+
+    public static ToDo newInstance() {
+        return new ToDo();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ToDoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/collegescheduler/ui/ToDo/ToDoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.collegescheduler.ui.ToDo;
+
+import androidx.lifecycle.ViewModel;
+
+public class ToDoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
