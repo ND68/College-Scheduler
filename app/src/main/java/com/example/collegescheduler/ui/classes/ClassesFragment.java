d57bc190af69034f4c215fb1da766547be7c25ce
Nathan <nathandong68@gmail.com>
2024-01-30 22:58:51 -0500

    Edited ListView ID in classes

--- app/src/main/java/com/example/collegescheduler/ui/classes/ClassesFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/classes/ClassesFragment.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.collegescheduler.R;
 import com.example.collegescheduler.databinding.FragmentClassesBinding;
+
+import java.util.ArrayList;
 
 public class ClassesFragment extends Fragment {
 
     private FragmentClassesBinding binding;
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +30,12 @@ public class ClassesFragment extends Fragment {
 
         binding = FragmentClassesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        listView = root.findViewById(R.id.listViewClasses);
+        items = new ArrayList<>();
+        items.add("CS 2340\n1/8/24 - 4/28/24");
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
 
         return root;
     }
