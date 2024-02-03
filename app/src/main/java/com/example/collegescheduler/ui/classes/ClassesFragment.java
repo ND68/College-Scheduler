830b80d720f888446c24c361c38fd3e482bbea74
Nathan <nathandong68@gmail.com>
2024-02-03 17:10:00 -0500

    Popup Edit fully implemented in ClassFragment
    Created class object, fully implemented edit feature

--- app/src/main/java/com/example/collegescheduler/ui/classes/ClassesFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/classes/ClassesFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -18,7 +17,6 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.collegescheduler.R;
 import com.example.collegescheduler.databinding.FragmentClassesBinding;
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
@@ -26,8 +24,33 @@ public class ClassesFragment extends Fragment implements AdapterView.OnItemLongC
 
     private FragmentClassesBinding binding;
     ListView listView;
-    ArrayList<String> items;
-    ArrayAdapter<String> adapter;
+    ArrayList<Class> classes;
+    ArrayAdapter<Class> adapter;
+
+    public class Class {
+        String section;
+        String dateStart;
+        String dateEnd;
+        String time;
+        String days;
+        String location;
+        String instructor;
+
+        public Class(String section, String dateStart, String dateEnd, String time, String days, String location, String instructor) {
+            this.section = section;
+            this.dateStart = dateStart;
+            this.dateEnd = dateEnd;
+            this.time = time;
+            this.days = days;
+            this.location = location;
+            this.instructor = instructor;
+        }
+        @Override
+        public String toString() {
+            return String.format("Section: %s\nDate Range: %s - %s\nTime: %s\nDays: %s\nLocation: %s\nInstructor: %s",
+                    section, dateStart, dateEnd, time, days, location, instructor);
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,16 +61,17 @@ public class ClassesFragment extends Fragment implements AdapterView.OnItemLongC
         View root = binding.getRoot();
 
         listView = root.findViewById(R.id.listViewClasses);
-        items = new ArrayList<>();
-        items.add("CS 2340\n1/8/24 - 4/28/24");
-        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        classes = new ArrayList<>();
+        classes.add(new Class("CS2340", "1/8/24", "4/28/24", "12:00",
+                "Wednesday", "Skiles 241", "Professor Pedro"));
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, classes);
         listView.setAdapter(adapter);
         listView.setOnItemLongClickListener(this);
 
         return root;
     }
 
-    public void editButtonPopup(View view, String details, int position) {
+    public void editButtonPopup(View view, Class classObject, int position) {
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.edit_popup, null);
         final EditText sectionInput = alertLayout.findViewById(R.id.editTextSectionCP);
@@ -58,18 +82,41 @@ public class ClassesFragment extends Fragment implements AdapterView.OnItemLongC
         final EditText locationInput = alertLayout.findViewById(R.id.editTextLocationCP);
         final EditText instructorInput = alertLayout.findViewById(R.id.editTextInstructorCP);
 
+        EditText[] inputs = {sectionInput, dateStartInput, dateEndInput, timeInput, daysInput, locationInput, instructorInput};
+        String[] classInfo = {classObject.section, classObject.dateStart, classObject.dateEnd, classObject.time, classObject.days,
+                        classObject.location, classObject.instructor};
+
+        for (int idx = 0; idx < inputs.length; idx++) {
+            inputs[idx].setText(classInfo[idx]);
+        }
+
         AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
         alert.setTitle("Edit Class");
         // this is set the view from XML inside AlertDialog
         alert.setView(alertLayout);
         // disallow cancel of AlertDialog on click of back button and outside touch
         alert.setCancelable(false);
-        alert.setNegativeButton("Cancel", (dialog, which) -> Toast.makeText(getContext(), "Cancel clicked", Toast.LENGTH_SHORT).show());
+        alert.setNegativeButton("Cancel", (dialog, which) -> Toast.makeText(getContext(), "Edit Canceled", Toast.LENGTH_SHORT).show());
 
         alert.setPositiveButton("Edit", (dialog, which) -> {
-            String user = sectionInput.getText().toString();
-            String pass = instructorInput.getText().toString();
-            Toast.makeText(getContext(), "Class Edited", Toast.LENGTH_LONG).show();
+            String section = sectionInput.getText().toString().trim();
+            String dateStart = dateStartInput.getText().toString().trim();
+            String dateEnd = dateEndInput.getText().toString().trim();
+            String time = timeInput.getText().toString().trim();
+            String days = daysInput.getText().toString().trim();
+            String location = locationInput.getText().toString().trim();
+            String instructor = instructorInput.getText().toString().trim();
+
+            if (section.isEmpty() || dateStart.isEmpty() || dateEnd.isEmpty() || time.isEmpty() ||
+                    days.isEmpty() || location.isEmpty() || instructor.isEmpty()) {
+                Toast.makeText(getContext(), "Cannot Have Empty Entries", Toast.LENGTH_LONG).show();
+            } else {
+                classes.set(position, new Class(section, dateStart, dateEnd, time, days, location, instructor));
+                adapter.notifyDataSetChanged();
+                Toast.makeText(getContext(), "Class Edited", Toast.LENGTH_LONG).show();
+            }
+
+
         });
         AlertDialog dialog = alert.create();
         dialog.show();
@@ -82,8 +129,8 @@ public class ClassesFragment extends Fragment implements AdapterView.OnItemLongC
 
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        String classDetails = items.get(position);
-        editButtonPopup(view, classDetails, position);
+        Class classObject = classes.get(position);
+        editButtonPopup(view, classObject, position);
 
         return false;
     }
